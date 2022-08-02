8b489daf527242e0bb2357c1727aec9942167f2b
David Röger <david@codeatelier.com>
2022-08-02 21:19:02 +0200

    Add tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import Head from 'next/head'
 
+import './app.css';
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 
 export default function App(props) {
   return (
-    <View style={styles.container}>
-      <Text accessibilityRole="header" style={styles.text}>
-        React Native for Web & Next.js
-      </Text>
+    <>         
+    <h1 className="text-3xl font-bold underline">
+      HTML H1 element
+    </h1>
 
-      <Text style={styles.link} accessibilityRole="link" href={`/alternate`}>
-        A universal link
+    <View className="h-full flex justify-center items-center">
+      <Text accessibilityRole="header">
+        React Native (web) Text Element
       </Text>
-
-      <View style={styles.textContainer}>
-        <Text accessibilityRole="header" aria-level="2" style={styles.text}>
-          Subheader
-        </Text>
-      </View>
     </View>
+    </>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flexGrow: 1,
-    justifyContent: 'center',
-  },
-  link: {
-    color: 'blue',
-  },
-  textContainer: {
-    alignItems: 'center',
-    marginTop: 16,
-  },
-  text: {
-    alignItems: 'center',
-    fontSize: 24,
-    marginBottom: 24,
-  },
-})
